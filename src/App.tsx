d9cbf61c553adedcc72d99b56d09ff8523ae103a
cankulpedin <cankulpedin@gmail.com>
2020-12-14 16:41:10 +0300

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledDiv = styled.div`
-    margin-left: 4px;
-`
+  margin-left: 4px;
+`;
 
 export const App = (): JSX.Element => (
-    <StyledDiv>
-        Welcome to React App
-    </StyledDiv>
-)
+  <StyledDiv>Welcome to React App</StyledDiv>
+);
