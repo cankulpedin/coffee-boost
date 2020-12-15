7986f8dfc7cfed4f199cd1c58c61e6aac43dc501
cankulpedin <cankulpedin@gmail.com>
2020-12-15 13:56:27 +0300

    feat: add route names constant

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import ROUTE_NAMES from "./routeNames";
+
 export const Routes = (): JSX.Element => {
   return (
     <Switch>
-      <Route exact path="/">
+      <Route exact path={ROUTE_NAMES.ROOT}>
         TODO
       </Route>
-      <Route exact path="/about">
+      <Route exact path={ROUTE_NAMES.ABOUT}>
         ABOUT
       </Route>
     </Switch>
---
+++ src/routes/routeNames.ts
@@ -0,0 +1,10 @@
+const ROOT = "/"
+
+const ABOUT = `${ROOT}about`
+
+const ROUTE_NAMES = {
+    ROOT,
+    ABOUT
+};
+
+export default ROUTE_NAMES;
