86d14763381fc01d4816cdd30ee916b3be77822f
cankulpedin <cankulpedin@gmail.com>
2020-12-24 16:56:45 +0300

    chore: add environment config to webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,37 +4,41 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const TS_EXTENSION = /\.(ts|tsx)$/;
 
-module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
-  target: "web",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: TS_EXTENSION,
-        loader: "eslint-loader",
-        exclude: /node_modules/,
-        enforce: "pre",
-      },
-      {
-        test: TS_EXTENSION,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
+module.exports = (env) => {
+  const envConfig = env.config;
+
+  return {
+    entry: "./src/index.tsx",
+    mode: envConfig === "dev" ? "development" : "production",
+    target: "web",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: TS_EXTENSION,
+          loader: "eslint-loader",
+          exclude: /node_modules/,
+          enforce: "pre",
+        },
+        {
+          test: TS_EXTENSION,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html"),
-    }),
-  ],
+  };
 };
 
 /* eslint-enable @typescript-eslint/no-var-requires */
